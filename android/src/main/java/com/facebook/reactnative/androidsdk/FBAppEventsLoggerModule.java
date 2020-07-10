161d2abe86165d968f82a081d02850f81c382afe
KarlosQ <carlos.quiroga.informatica@gmail.com>
2020-07-10 15:18:24 +0200

    fix android bundle

--- android/src/main/java/com/facebook/reactnative/androidsdk/FBAppEventsLoggerModule.java
+++ android/src/main/java/com/facebook/reactnative/androidsdk/FBAppEventsLoggerModule.java
@@ -17,8 +17,8 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 package com.facebook.reactnative.androidsdk;
+import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
